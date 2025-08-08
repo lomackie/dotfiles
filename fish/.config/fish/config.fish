c4460ebcebcc3abde6aab3cb970e9ec3b32de960
Louis Mackie <me@louismackie.com>
2025-08-08 18:46:28 +0000

    Mac vs Linux path conf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,15 @@
 # Global settings
 set -gx EDITOR nvim
-set -gx PATH /opt/homebrew/bin /opt/homebrew/opt/make/libexec/gnubin $HOME/go/bin $PATH
+
+if test (uname) = "Darwin"
+    set -gx PATH /opt/homebrew/bin /opt/homebrew/opt/make/libexec/gnubin $PATH
+else if test (uname) = "Linux"
+    set -gx PATH /opt/nvim-linux-x86_64/bin $PATH
+end
+
+# Common path
+set -gx PATH $HOME/go/bin $PATH
+
 starship init fish | source
 
 source $HOME/.config/fish/conf.d/abbr.fish
