bd589877c2756535ba735d3684e1cfd0a6ae7c30
lomackie <me@louismackie.com>
2025-10-09 20:38:02 +0100

    Add better git commit abbrs

--- fish/.config/fish/functions/cdg.fish
+++ fish/.config/fish/functions/cdg.fish
@@ -1,10 +1,3 @@
-# cdg: jump (zoxide) to the repo root if inside a git repo, else normal z
-functions -e cdg ^/dev/null
 function cdg
-    if git rev-parse --is-inside-work-tree ^/dev/null
-        set -l root (git rev-parse --show-toplevel ^/dev/null)
-        z --basedir $root
-    else
-        z
-    end
+    z --basedir $(git rev-parse --show-toplevel)
 end
